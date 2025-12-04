2ad1021ed16e0c03da689bc507541280f0ca6c71
o7digital <101292703+o7digital@users.noreply.github.com>
2025-12-04 09:21:46 -0600

    chore: remove vercel adapter for netlify deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://utopica.net',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
   integrations: [
     react(),
     tailwind({
